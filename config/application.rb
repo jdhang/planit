aa8488a1e865afb8e730f2fc6a8d8097092feeab
Jason Hang <jason.d.hang@gmail.com>
2014-11-20 23:12:40 -0500

    Asset pipeline in background image

--- config/application.rb
+++ config/application.rb
@@ -19,9 +19,5 @@ module Planit
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.serve_static_assets = true
-    config.assets.precompile += [
-        'bg2.jpg'
-    ]
   end
 end
